09a6babf9c62adde2ea6e7eb6c90dea60c5702ab
tspch02 <tspch02@tspch02ui-MacBook-Pro.local>
2021-06-20 22:03:00 +0900

    feat: containter_name, ports

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+export DB_CONTAINER_NAME="postgres_db"
+export ADMIN_CONTAINER_NAME="postgres_admin"
 export DB_PASSWORD="password"
 export DB_DBNAME="database"
 export DB_USERNAME="postgres"
+export DB_PORT=5432
+export ADMIN_PORT=8080
 docker-compose up -d
